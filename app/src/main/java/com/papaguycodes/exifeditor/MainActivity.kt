6fce9d467fd38678dc53564eae545243e0f5df1a
PaPaGuy Codes  / <175421249+PaPaGuyCodes@users.noreply.github.com>
2024-08-11 18:08:59 +0100

    Update MainActivity.kt

--- app/src/main/java/com/papaguycodes/exifeditor/MainActivity.kt
+++ app/src/main/java/com/papaguycodes/exifeditor/MainActivity.kt
@@ -1,22 +1,24 @@
 package com.papaguycodes.exifeditor
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.papaguycodes.exifeditor.databinding.ActivityMainBinding
 import com.papaguycodes.exifeditor.utils.ThemeUtils
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ThemeUtils.applyTheme(this)  // Apply the GitHub-inspired theme
-        setContentView(R.layout.activity_main)
 
-        // Open the ExifEditor when the user clicks a button (assume button has been set in XML)
-        findViewById<Button>(R.id.openExifEditorButton).setOnClickListener {
-            val intent = Intent(this, ExifEditor::class.java)
-            startActivity(intent)
-        }
+        // Applying Theme
+        ThemeUtils.applyTheme(this)
+
+        // Setting up Data Binding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Implement additional UI logic here
     }
 }
-
