ffa46e806837c7f00a8e56e760a7fa495ee64b19
PaPaGuy Codes  / <175421249+PaPaGuyCodes@users.noreply.github.com>
2024-08-11 22:00:35 +0100

    Create ThemeUtils.kt

---
+++ app/src/main/java/com/papaguycodes/utils/ThemeUtils.kt
@@ -0,0 +1,17 @@
+package com.papaguycodes.exifeditor.utils
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import com.papaguycodes.exifeditor.R
+
+object ThemeUtils {
+
+    fun applyTheme(context: Context) {
+        val sharedPrefs = context.getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPrefs.getBoolean("dark_mode", true)
+        
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+        )
+    }
+}
