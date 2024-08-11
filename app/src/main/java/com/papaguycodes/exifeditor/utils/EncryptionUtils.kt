439d04d10db914b1eb5c213fde20a8aa27be5d4e
PaPaGuy Codes  / <175421249+PaPaGuyCodes@users.noreply.github.com>
2024-08-11 17:59:47 +0100

    Update EncryptionUtils.kt

--- app/src/main/java/com/papaguycodes/exifeditor/utils/EncryptionUtils.kt
+++ app/src/main/java/com/papaguycodes/exifeditor/utils/EncryptionUtils.kt
@@ -1,37 +1,40 @@
 package com.papaguycodes.exifeditor.utils
 
+import android.util.Base64
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
-import javax.crypto.spec.IvParameterSpec
-import java.util.Base64
+import javax.crypto.spec.SecretKeySpec
 
 object EncryptionUtils {
 
-    private const val ALGORITHM = "AES/CBC/PKCS5Padding"
-    private const val KEY_SIZE = 256
+    private const val AES = "AES"
 
-    fun generateSecretKey(): SecretKey {
-        val keyGen = KeyGenerator.getInstance("AES")
-        keyGen.init(KEY_SIZE)
+    fun generateKey(): SecretKey {
+        val keyGen = KeyGenerator.getInstance(AES)
+        keyGen.init(256)
         return keyGen.generateKey()
     }
 
-    fun encrypt(data: String, secretKey: SecretKey, iv: IvParameterSpec): String {
-        val cipher = Cipher.getInstance(ALGORITHM)
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv)
-        return Base64.getEncoder().encodeToString(cipher.doFinal(data.toByteArray()))
+    fun encrypt(data: String, secretKey: SecretKey): String {
+        val cipher = Cipher.getInstance(AES)
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+        val encryptedData = cipher.doFinal(data.toByteArray())
+        return Base64.encodeToString(encryptedData, Base64.DEFAULT)
     }
 
-    fun decrypt(encryptedData: String, secretKey: SecretKey, iv: IvParameterSpec): String {
-        val cipher = Cipher.getInstance(ALGORITHM)
-        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv)
-        return String(cipher.doFinal(Base64.getDecoder().decode(encryptedData)))
+    fun decrypt(encryptedData: String, secretKey: SecretKey): String {
+        val cipher = Cipher.getInstance(AES)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey)
+        val decodedData = Base64.decode(encryptedData, Base64.DEFAULT)
+        return String(cipher.doFinal(decodedData))
     }
 
-    fun generateIv(): IvParameterSpec {
-        val iv = ByteArray(16)
-        java.security.SecureRandom().nextBytes(iv)
-        return IvParameterSpec(iv)
+    fun secretKeyFromString(key: String): SecretKey {
+        return SecretKeySpec(Base64.decode(key, Base64.DEFAULT), AES)
+    }
+
+    fun secretKeyToString(secretKey: SecretKey): String {
+        return Base64.encodeToString(secretKey.encoded, Base64.DEFAULT)
     }
 }
